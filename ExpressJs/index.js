aaa2c8106c538749f538f72c2f03c87a1db48f12
MezliniH <die.mezlini@gmail.com>
2022-01-19 12:16:42 +0100

    expressJs checkpoint 0.2.0

--- ExpressJs/index.js
+++ ExpressJs/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(
+  (closingTime = (req, res, next) => {
+    let hours = new Date().getHours();
+    let day = new Date().getDay();
+    if ((hours <= 9) || (hours >= 17) || (day == 0) || (day == 6)) {
+      res.sendFile(__dirname + "/views/404.html");
+    } else {
+      next();
+    }
+  })
+);
+
 app.use(express.static("views"));
 
-const port = 5000;
+const port =  5000;
 app.listen(port, (err) => {
   if (err) {
     console.log("server is not running")
